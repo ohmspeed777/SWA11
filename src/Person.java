c59dabf29d279d1097a1871606d0224505d94e4a
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-09-13 15:22:02 +0700

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+
+	private Vehicle vehicle;
+	
+	public Person(Vehicle v) {
+		vehicle = v;
+	}
+	
+	public void setDrive(Vehicle v) {
+		vehicle = v;
+	}
+	
+	public void drive() {
+		vehicle.drive();
+	}
+
+}
---
+++ src/Rocket.java
@@ -0,0 +1,10 @@
+
+public class Rocket extends Vehicle {
+
+	@Override
+	public void drive() {
+		// TODO Auto-generated method stub
+		System.out.println("Rocket Car");
+	}
+
+}
